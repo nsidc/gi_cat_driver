b48a9f568fb5dafcfd581b37d107d29cfdb552a1
Stuart Reed <stuart.reed@nsidc.org>
2013-03-13 12:26:17 -0600

    Refactored test organization and readability

--- spec/esip_opensearch_query_builder_spec.rb
+++ spec/esip_opensearch_query_builder_spec.rb
@@ -2,18 +2,21 @@ require 'spec_helper'
 require 'esip_opensearch_query_builder'
 
 describe EsipOpensearchQueryBuilder do
-  it "Returns an ESIP OpenSearch URL with empty parameters for the default query" do
-    query = EsipOpensearchQueryBuilder::get_query_string()
-    query.should eq "?si=&ct=&st=&bbox=&rel=&loc=&ts=&te=&lac=&luc=&outputFormat="
-  end
 
-  it "Returns an ESIP OpenSearch URL with a bounding box constraint given a bbox parameter" do
-    query = EsipOpensearchQueryBuilder::get_query_string( :bbox => 'abcd')
-    query.should eq "?si=&ct=&st=&bbox=abcd&rel=&loc=&ts=&te=&lac=&luc=&outputFormat="
-  end
+  describe "get_query_string returns ESIP OpenSearch URLs" do
+    it "Returns a URL with empty parameters when called without arguments" do
+      query = EsipOpensearchQueryBuilder::get_query_string()
+      query.should eq "?si=&ct=&st=&bbox=&rel=&loc=&ts=&te=&lac=&luc=&outputFormat="
+    end
 
-  it "Returns an ESIP OpenSearch URL with a search term given a st parameter" do
-    query = EsipOpensearchQueryBuilder::get_query_string( :st => 'snow')
-    query.should eq "?si=&ct=&st=snow&bbox=&rel=&loc=&ts=&te=&lac=&luc=&outputFormat="
+    it "Returns a URL with a bounding box when called with a bbox argument" do
+      query = EsipOpensearchQueryBuilder::get_query_string( :bbox => 'abcd')
+      query.should eq "?si=&ct=&st=&bbox=abcd&rel=&loc=&ts=&te=&lac=&luc=&outputFormat="
+    end
+
+    it "Returns a URL with a search term when called with a st argument" do
+      query = EsipOpensearchQueryBuilder::get_query_string( :st => 'snow')
+      query.should eq "?si=&ct=&st=snow&bbox=&rel=&loc=&ts=&te=&lac=&luc=&outputFormat="
+    end
   end
 end
--- spec/gi_cat_driver_spec.rb
+++ spec/gi_cat_driver_spec.rb
@@ -12,37 +12,43 @@ describe GiCatDriver do
     @gi_cat.base_url.should eq(expected)
   end
 
-  it "Is able to send requests to GI-Cat" do
-    @gi_cat.is_running?.should be_true
+  describe "Standard requests" do
+    it "Is able to send requests to GI-Cat" do
+      @gi_cat.is_running?.should be_true
+    end
+
+    it "Sends requests with basic header information" do
+      @gi_cat.standard_headers[:content_type].should eq "application/xml"
+    end
+
+    it "Can authorize access using the Authorization header" do
+      @gi_cat.standard_headers[:Authorization].should eq "Basic YWRtaW46YWJjZDEyMyQ="
+    end
   end
 
-  it "Sends requests with basic header information" do
-    @gi_cat.standard_headers[:content_type].should eq "application/xml"
+  describe "Profile management" do
+    it "Retrieves a profile id given the name" do
+      @gi_cat.find_profile_id("NORWEGIAN_CISL_NSIDC_EOL").should eq "1"
+    end
+
+    it "Throws an error if the profile cannot be found" do
+      @gi_cat.find_profile_id("notaprofile").should be_nil
+    end
+
+    it "Enables a profile given the name" do
+      @gi_cat.enable_profile("NORWEGIAN_CISL_NSIDC_EOL")
+      @gi_cat.get_active_profile_id.should eq "1"
+    end
+
+    it "Does not enable a profile if the profile cannot be found" do
+      expect { @gi_cat.enable_profile("notaprofile") }.to raise_error("The specified profile could not be found.")
+    end
   end
 
-  it "Can authorize access using the Authorization header" do
-    @gi_cat.standard_headers[:Authorization].should eq "Basic YWRtaW46YWJjZDEyMyQ="
-  end
-
-  it "Retrieves a profile id given the name" do
-    @gi_cat.find_profile_id("NORWEGIAN_CISL_NSIDC_EOL").should eq "1"
-  end
-
-  it "Throws an error if the profile cannot be found" do
-    @gi_cat.find_profile_id("notaprofile").should be_nil
-  end
-
-  it "Enables a profile given the name" do
-    @gi_cat.enable_profile("NORWEGIAN_CISL_NSIDC_EOL")
-    @gi_cat.get_active_profile_id.should eq "1"
-  end
-
-  it "Does not enable a profile if the profile cannot be found" do
-    expect { @gi_cat.enable_profile("notaprofile") }.to raise_error("The specified profile could not be found.")
-  end
-
-  it "Enables Lucene for the active profile" do
-    @gi_cat.enable_lucene
-    @gi_cat.is_lucene_enabled?.should be_true
+  describe "Lucene Indexing" do
+    it "Enables Lucene for the active profile" do
+      @gi_cat.enable_lucene
+      @gi_cat.is_lucene_enabled?.should be_true
+    end
   end
 end
