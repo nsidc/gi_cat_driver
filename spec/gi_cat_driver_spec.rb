8b4f4f8cf23ed87fd25ce44bc90ee07b3f085d04
Stuart Reed <stuart.reed@nsidc.org>
2013-03-14 10:14:51 -0600

    Commented out tests dependent on live application

--- spec/gi_cat_driver_spec.rb
+++ spec/gi_cat_driver_spec.rb
@@ -3,7 +3,7 @@ require 'gi_cat_driver'
 
 describe GiCatDriver do
   before(:each) do
-    @base_url = ENV["URL"] or throw "Error: Must provide URL environment variable for GI-Cat!"
+    @base_url = "http://www.somecompany.com/"
     @gi_cat = GiCatDriver::GiCat.new(@base_url, "admin", "abcd123$")
   end
 
@@ -13,11 +13,11 @@ describe GiCatDriver do
   end
 
   describe "Standard requests" do
-    it "Is able to send requests to GI-Cat" do
-      @gi_cat.is_running?.should be_true
-    end
+    #it "Is able to send requests to GI-Cat" do
+      #@gi_cat.is_running?.should be_true
+    #end
 
-    it "Sends requests with basic header information" do
+   it "Sends requests with basic header information" do
       @gi_cat.standard_headers[:content_type].should eq "application/xml"
     end
 
@@ -26,29 +26,29 @@ describe GiCatDriver do
     end
   end
 
-  describe "Profile management" do
-    it "Retrieves a profile id given the name" do
-      @gi_cat.find_profile_id("NORWEGIAN_CISL_NSIDC_EOL").should eq "1"
-    end
+  #describe "Profile management" do
+    #it "Retrieves a profile id given the name" do
+      #@gi_cat.find_profile_id("NORWEGIAN_CISL_NSIDC_EOL").should eq "1"
+    #end
 
-    it "Throws an error if the profile cannot be found" do
-      @gi_cat.find_profile_id("notaprofile").should be_nil
-    end
+    #it "Throws an error if the profile cannot be found" do
+      #@gi_cat.find_profile_id("notaprofile").should be_nil
+    #end
 
-    it "Enables a profile given the name" do
-      @gi_cat.enable_profile("NORWEGIAN_CISL_NSIDC_EOL")
-      @gi_cat.get_active_profile_id.should eq "1"
-    end
+    #it "Enables a profile given the name" do
+      #@gi_cat.enable_profile("NORWEGIAN_CISL_NSIDC_EOL")
+      #@gi_cat.get_active_profile_id.should eq "1"
+    #end
 
-    it "Does not enable a profile if the profile cannot be found" do
-      expect { @gi_cat.enable_profile("notaprofile") }.to raise_error("The specified profile could not be found.")
-    end
-  end
+    #it "Does not enable a profile if the profile cannot be found" do
+      #expect { @gi_cat.enable_profile("notaprofile") }.to raise_error("The specified profile could not be found.")
+    #end
+  #end
 
-  describe "Lucene Indexing" do
-    it "Enables Lucene for the active profile" do
-      @gi_cat.enable_lucene
-      @gi_cat.is_lucene_enabled?.should be_true
-    end
-  end
+  #describe "Lucene Indexing" do
+    #it "Enables Lucene for the active profile" do
+      #@gi_cat.enable_lucene
+      #@gi_cat.is_lucene_enabled?.should be_true
+    #end
+  #end
 end
